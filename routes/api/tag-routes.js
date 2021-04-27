c55588011cb69edb9a83a1678bc9781714b33ea0
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-04-27 13:41:20 -0700

    updated include in tag-routes GET request for all products so that it goes through ProductTag as tagged_products; in tag-routes file I created a code block for the GET request for a single tag by the id the user entered including its association with Product data through ProductTag as tagged_products, if tag not found then user is presented with a 404 status error message and returned, otherwise they are given the data they wanted, if error occurs they are presented with a 500 status error message.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,18 +8,32 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag, as: 'tagged_products'}]
     });
     res.status(200).json(tagData);
-    
+
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'tagged_products'}]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!'});
+      return;
+    };
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
