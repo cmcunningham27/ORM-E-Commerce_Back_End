83bd46747bf8410953a4814e658289ace39b6d18
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-04-28 12:24:31 -0700

    in tag-routes file I craeted a code block for the UPDATE request that responds by updating the tag name by its id value that the user enters, if no tag is found the user will be given a 404 status message, if initially there is no req.body for the tag they will be presented with a 400 error message, otherwise they will be able to check that the update to the tag name has been made, if an error occurs the user will be presented with a 500 status error.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,12 +46,49 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    if (!req.body) {
+      res.status(400).json({ message: 'Could not update!'})
+    };
+
+    const tagData = await Tag.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id,
+        }
+      }
+    );
+
+    if (!tagData[0]) {
+      res.status(404).json({ mesage: 'Tag not found!' });
+    };
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'Tag not found!' });
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
